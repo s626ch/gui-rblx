485a0c242a58cdca7790877d48ad1c8e1f1cbf1f
Gilbey_Gage <Gilbey_Gage@student.mahoningctc.com>
2021-09-08 12:39:20 -0400

    more stupid stuff

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import tkinter as tk  # library to build GUI application
 
 # start building window 
 window = tk.Tk()
+# window items
+mainLabel = tk.Label(
+    text="Example GUI application to interact with ROBLOX."
+)
+
+# functions
+
+# pack
+mainLabel.pack()
+
+# initialize window
+window.mainloop()
